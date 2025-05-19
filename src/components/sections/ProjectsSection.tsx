90a9a404af779dc7f6d4eaf2df2b12f56ef7ad31
unstableme <unstableme02@gmail.com>
2025-05-19 18:09:44 +0545

    new two proj

--- src/components/sections/ProjectsSection.tsx
+++ src/components/sections/ProjectsSection.tsx
@@ -6,7 +6,28 @@ import { ExternalLink, Github, X } from "lucide-react";
 
 const projectsData = [
   {
-    id: 1,
+  id: 1,
+    title: "Breast Cancer Prediction",
+    description: "Predict whether someone has breast cancer or not with the help of Neural Networks.",
+    image: "/Breast Cancer.jpg?auto=format&fit=crop&w=800&q=80",
+    tags: ["Python", "Machine Learning", "Neural Networks", "Deep Learning"],
+    demoUrl: "#",
+    repoUrl: "https://github.com/unstableme/Breast_cancer_UCI_NN/blob/main/breast_cancer_uci.ipynb",
+    fullDescription: "I used neural networks (deep learning) to classify based on UCI dataset whether someone has breast cancer or not."
+  },
+  {
+    id: 2,
+    title: "Medical Appointment Shows/No-Shows",
+    description: "Analyze and finds out whether someone after booking a date, shows up in appointment or not.",
+    image: "/Medical Appointment.jpg?auto=format&fit=crop&w=800&q=80",
+    tags: ["Python", "Machine Learning", "Classification", "Logistic Regression",'Random Forest', 'Boosting Algorithm', 'Bagging Algorithm'],
+    demoUrl: "#",
+    repoUrl: "https://github.com/unstableme/Medical_appointment_noshows-shows/blob/main/appointment_show.ipynb",
+    fullDescription: "I used different classification algorithms like logistic regression, different boosting and bagging algorithms to classify the data whether someone shows up in the scheduled date or not and determined which variable affects the most."
+  },
+
+  {
+    id: 3,
     title: "Intelligent Diet Planner",
     description: "Recommends diet to user based on their workout & goal.",
     image: "/IDP.jpeg?auto=format&fit=crop&w=800&q=80",
@@ -16,7 +37,7 @@ const projectsData = [
     fullDescription: "It is a website made using the Django framework and trained with a Machine Learning algorithm that provides meal options for users according to their requirements."
   },
   {
-    id: 2,
+    id: 4,
     title: "Visa Requirement Prediction",
     description: "A model to predict whether the country requires visa or not.",
     image: "/Visa Req Predict.jpeg?auto=format&fit=crop&w=800&q=80",
@@ -26,7 +47,7 @@ const projectsData = [
     fullDescription: "I used Classification ML algorithm to predict whether traveling from country A to country B requires a visa or not."
   },
   {
-    id: 3,
+    id: 5,
     title: "Laptop Price Prediction",
     description: "A model to predict different laptop's price based on their specification like name, model, storage, etc.",
     image: "/Laptop Price Predict.jpeg?auto=format&fit=crop&w=800&q=80",
@@ -36,7 +57,7 @@ const projectsData = [
     fullDescription: "I used different Machine Learning models to predict the Price of Laptop on the basis of different features including RAM, GPU, Manufacturer, Storage etc."
   },
   {
-    id: 4,
+    id: 6,
     title: "Book Detail Scraping",
     description: "Used Python package to scrap the book details from website.",
     image: "/Book scraping.jpeg?auto=format&fit=crop&w=800&q=80",
@@ -46,7 +67,7 @@ const projectsData = [
     fullDescription: "I performed webscraping using BeautifulSoup and Request module using Python and exported in Excel sheets."
   },
   {
-    id: 5,
+    id: 7,
     title: "Unstable Photography Gallery",
     description: "Gallery for organizing my photographs",
     image: "/Photograph gallery.jpeg?auto=format&fit=crop&w=800&q=80",
